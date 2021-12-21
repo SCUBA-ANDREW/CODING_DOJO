ea2b664124f2121d68450ddb4202153dd98038a1
Andrew Lee <andrewlee0807@gmail.com>
2021-12-20 17:44:11 -0800

    Likes assignment

---
+++ Web_Fundamentals/JS/Likes_assignment/script.js
@@ -0,0 +1,21 @@
+var JR_count = 9
+var JR_like = document.querySelector("#like_CountJR")
+function JR_liking(){
+    JR_count++;
+    JR_like.innerText = "" + JR_count + " like (s)";
+}
+
+
+var NM_count = 9
+var NM_like = document.querySelector("#like_CountNM")
+function NM_liking(){
+    NM_count++;
+    NM_like.innerText = "" + NM_count + " like (s)";
+}
+
+var NK_count = 12
+var NK_like = document.querySelector("#like_CountNK")
+function NK_liking(){
+    NK_count++;
+    NK_like.innerText = "" + NK_count + " like (s)";
+}
