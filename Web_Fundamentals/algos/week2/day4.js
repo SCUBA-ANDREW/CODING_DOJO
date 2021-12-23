ce299c7851a2744ef37efddeea6c7e1ae960693b
Andrew Lee <andrewlee0807@gmail.com>
2021-12-23 10:44:30 -0800

    algo week2 day4

--- Web_Fundamentals/algos/week2/day4.js
+++ Web_Fundamentals/algos/week2/day4.js
@@ -21,6 +21,11 @@ function isPresent2d(arr2d, value){
 // complete the following function
 function flatten(arr2d) {
     var flat = [];
+    for(i=0;i<arr2d.length;i++){
+        for(j=0;j<arr2d[i].length;j++){
+            flat.push(arr2d[i][j]);
+        }
+    }
     // your code here
     return flat;
 }
