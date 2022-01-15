a20e3ab2ab9febe6f1bb7685bd2f1b60a93bf117
Andrew Lee <andrewlee0807@gmail.com>
2022-01-14 19:11:41 -0800

    counter assignment

--- python/flask/fundamentals/Counter/server.py
+++ python/flask/fundamentals/Counter/server.py
@@ -1,16 +1,43 @@
-import re
-from flask import Flask,request,redirect,session
-from flask.templating import render_template
+
+from flask import Flask,request,redirect,session,render_template
+
 
 app = Flask(__name__)
+app.secret_key='ahhh'
+
+
 
 @app.route('/')
-def homepage(count=0):
-    return render_template('index.html',count=count)
+def index():
+    print("test")
+    if 'visit_session' not in session:
+        session['visit_session']=1
+    else:
+        session['visit_session']+=1
+    if 'count_session' not in session:
+        session['count_session']=1
+    else:
+        session['count_session']+=1
+    return render_template('index.html',count_var=session['count_session'],vis_count=session['visit_session'])
+
+@app.route('/count_page',methods=['POST'])
+def count():
+    print("test count page")
+    # session['count_session'] =request.form['count_request']
+    session['count_session']+=1
+    return redirect("/")
 
 @app.route('/destroy_session')
+@app.route('/destroy_session', methods=['POST'])
 def clear():
-    # count=0
+    print("test clear page")
+    session.pop('count_session')
+    return redirect("/")
+
+@app.route('/input_count',methods=['POST'])
+def input_count():
+    print("test input count ")
+    session['count_session'] =int(request.form['input_count'])+int(session['count_session']-1)
     return redirect("/")
 
 
