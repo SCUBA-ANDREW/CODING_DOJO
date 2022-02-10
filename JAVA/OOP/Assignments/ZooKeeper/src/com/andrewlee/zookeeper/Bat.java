4a359d58454864c22fdef8f9415acd758ed6ca05
ANDREW LEE <94152578+SCUBA-ANDREW@users.noreply.github.com>
2022-02-09 21:16:47 -0800

    zoo keeper II

---
+++ JAVA/OOP/Assignments/ZooKeeper/src/com/andrewlee/zookeeper/Bat.java
@@ -0,0 +1,20 @@
+package com.andrewlee.zookeeper;
+
+public class Bat extends Mammal{
+	public Bat() {
+		this.setEnergyLevel(300);
+	}
+	public void fly() {
+		System.out.println("Flying and felt tired");
+		this.setEnergyLevel(this.getEnergyLevel()-50);
+	}
+	public void eatHuman() {
+		System.out.println("Ate a human and felt good");
+		this.setEnergyLevel(this.getEnergyLevel()+25);
+		
+	}
+	public void attackTown() {
+		System.out.println("whooooosh");
+		this.setEnergyLevel(this.getEnergyLevel()-100);
+	}
+}
