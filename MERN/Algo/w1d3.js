d7f9108fad1790c49e679b2a36834912aa4d0225
ANDREW LEE <94152578+SCUBA-ANDREW@users.noreply.github.com>
2022-03-09 09:27:31 -0800

    week 1 day 3 algorithm

--- MERN/Algo/w1d3.js
+++ MERN/Algo/w1d3.js
@@ -19,4 +19,23 @@
 */
 const myArr = [3,2,9,5,1,4,8]
 
-function selectionSort(arr){ }
+function selectionSort(arr){ 
+    for(let i=0;i<arr.length-1;i++){
+        let minIdx = i;
+        for(let j=i+1;j<arr.length;j++){
+            if(arr[j]<arr[minIdx]){
+                minIdx = j;
+            }
+        }
+        let temp = arr[minIdx];
+        arr[minIdx]=arr[i];
+        arr[i]=temp;
+    }
+    return arr;
+    
+}
+
+
+
+console.log(selectionSort(myArr));
+// console.log(myArr);
