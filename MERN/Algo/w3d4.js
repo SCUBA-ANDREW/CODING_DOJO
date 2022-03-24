abf01edad3c4ea83a92006cc56ae312f097297b1
ANDREW LEE <94152578+SCUBA-ANDREW@users.noreply.github.com>
2022-03-24 10:05:37 -0700

    MERN week3 day4 algorithm

--- MERN/Algo/w3d4.js
+++ MERN/Algo/w3d4.js
@@ -14,7 +14,7 @@ const medications = [
         ],
     },
     {
-        
+
         name: "Longvida Curcumin",
         treatableSymptoms: [
             "pain",
@@ -76,8 +76,62 @@ Output: []
 // then loop through second array to see if the symptoms match the inputted symptoms
 // if they match add them into a new array at the end return array
 // edge case if no matches return null
-function webMD(ailments, meds) { }
+function webMD(ailments, meds) {
+    let fin=[];//for return array
+    let rightMedCount = [];//temp storing the match sym count
+    let inCase=0;//in case nothing matches
+    for (med of meds) {//loop through every med in the medication list/array
+        let countSym = 0;//temp counting matching symptons per med
+        // console.log(med);
+        for (sym of med.treatableSymptoms) {//loop thorugh all the treatable syptoms for each med
+            for (key of ailments) {//loop trhough all the syptoms given in ailments
+                if (sym == key) {//check if sympton matches 
+                    countSym++;
+                }
+            }
+        }
+        rightMedCount.push(countSym);
+        inCase+=countSym;
+    }
+    if(inCase==0){//in
+        return [];
+    }
+    let hIdx = 0;
+    let hVal=0;
+    for(let i=0;i<rightMedCount.length;i++){
+        if(rightMedCount[i]>hVal){
+            hIdx=i;
+            hVal=rightMedCount[i];
+        }
+    }
+    for(let j=0;j<rightMedCount.length;j++){
+        if(rightMedCount[j]==hVal){
+            fin.push(meds[j].name)
+        }
+    }
+    return fin;
+
+
+}
+
+function webMD2(ailments,meds){
+    let fin=[];
+    for(sym of ailments){
+        console.log(sym);
+        for(med of meds){
+            console.log(med.treatableSymptoms);
+            if(med.treatableSymptoms.hasOwnProperty(sym)){
+                console.log("yes");
+            }
+        }
+
+    }
+}
 
 console.log(webMD(["pain"], medications));
 console.log(webMD(["pain", "inflammation", "depression"], medications));
 console.log(webMD(["existential dread"], medications));
+
+// console.log(webMD2(["pain"], medications));
+// console.log(webMD2(["pain", "inflammation", "depression"], medications));
+// console.log(webMD2(["existential dread"], medications));
